8942e38dbf3b42e7055d2b17b22ab6533ce5b27f
root <root@localhost.localdomain>
2012-05-22 11:17:55 +0400

    Реализация кнопки с подсветкой

---
+++ programmButton.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+
+"""
+
+"""
+
+import sys
+from PyQt4 import QtCore, QtGui, uic
+
+class QProgrammButton(QtGui.QPushButton):
+
+    def __init__(self, parent = None):
+        QtGui.QPushButton.__init__(self, parent)
+
