d6f676126b93faaa7c497f8e786cef0c1770a46d
Maksim Kolesnikov <maksim@m-200.com>
2012-06-25 13:35:10 +0400

    переделаны сообщения от кнопки

--- statusCmdEventFilter.py
+++ statusCmdEventFilter.py
@@ -13,32 +13,21 @@ def installEventFilter(widget):
 
     class statusCmdEventFilter(QtCore.QObject):
 
-	rightclicked = QtCore.pyqtSignal(int)
 	mouseleave = QtCore.pyqtSignal(int)
-	clicked = QtCore.pyqtSignal(int)
+	mouseenter = QtCore.pyqtSignal(int)
 	
 	def eventFilter(self, obj, event):
 
 	    if widget == obj:
-
-    		# событие мыш покинула кнопку
+    		# событие мышь покинула кнопку
     		if event.type() == QtCore.QEvent.Leave:
 		    self.mouseleave.emit(widget.numberButton)
         	    return True
-
-                # событие нажали кнопку мышки
-		if event.type() == QtCore.QEvent.MouseButtonRelease:
-                    # левая кнопка мышки нажата
-		    if event.button() & QtCore.Qt.LeftButton:
-        		self.clicked.emit(widget.numberButton)
-		        return True
-
-                    # правая кнопка мышки нажата
-		    if event.button() & QtCore.Qt.RightButton:
-        		self.rightclicked.emit(widget.numberButton)
-  		        return True
-
-  		    return False
+        	    
+        	# событие мышь навелась на кнопку
+        	if event.type() == QtCore.QEvent.Enter:
+		    self.mouseenter.emit(widget.numberButton)
+        	    return True
 
 	    return False
 	    
