b9467090afe8427e4121e4e04e32cd3376af0d00
Maksim Kolesnikov <maksim@m-200.com>
2012-07-11 12:20:14 +0400

    Проверено сборка под Windows работает

--- setup.py
+++ setup.py
@@ -1,6 +1,5 @@
-
 #!/usr/bin/python
-#coding=utf8
+# -*- coding: utf-8 -*-
 
 """
 Создание отдельно запускаемого проекта через py2exe
@@ -8,9 +7,19 @@
 
 
 from distutils.core import setup
+import glob
 import py2exe
+
+
+datafiles = [
+    ("ico",glob.glob("ico/*")),
+    (".", ["property.ui", "sipphone.ui"])
+]
 
 setup(
     windows=[{"script":"sipphone.py"}],
-    options={"py2exe": {"includes":["sip", "PyQt4", "pjsua"]}}
+    url='https://github.com/umkasuper/sip-phone',
+    data_files = datafiles,
+#    options={"py2exe": {"includes":["sip", "PyQt4", "pjsua", "statusCmdButton", "programmButton"], "bundle_files": 1,"compressed": 1}}
+    options={"py2exe": {"includes":["sip", "PyQt4", "pjsua", "statusCmdButton", "programmButton"]}}
 )
