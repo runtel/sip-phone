0f45823f77554bf647a2a1bc6584255f41fc3f43
Maksim Kolesnikov <maksim@m-200.com>
2012-08-22 16:52:16 +0400

    Изменены програмируемые кнопки

--- programmButton.py
+++ programmButton.py
@@ -10,13 +10,23 @@ from PyQt4 import QtCore, QtGui
 
 class QProgrammButton(QtGui.QPushButton):
     greenChanged = QtCore.pyqtSignal(bool)
-
+    
     def __init__(self, parent = None):
         super(QProgrammButton, self).__init__(parent)
-	self.number = -1
+        self.number = -1
         self.green = False
         self.red = False
+        self.opacity = 100
+        self.timer = QtCore.QTimer()
+        self.timer.setInterval(500)
+
+        QtCore.QObject.connect(self.timer, QtCore.SIGNAL("timeout()"), self.updateTimer)
+        
            
+    def updateTimer(self):
+        self.opacity = 100 if self.opacity == 50 else 50
+        self.update()
+
     def paintEvent(self, e):
         QtGui.QPushButton.paintEvent(self, e)
         qp = QtGui.QPainter()
@@ -25,32 +35,24 @@ class QProgrammButton(QtGui.QPushButton):
         qp.end()
 
     def drawWidget(self, qp):
-	pass
-#        size = self.size()
-#        w = size.width()
-#        h = size.height()
-       
-#        if self.green:
-#            pen = QtGui.QPen(QtGui.QColor(20, 20, 20), 10, 
-#                QtCore.Qt.SolidLine)
-#        else:
-#            pen = QtGui.QPen(QtGui.QColor(0, 255, 0), 10, 
-#                QtCore.Qt.SolidLine)
-            
-#        qp.setPen(pen)
-#        qp.setBrush(QtCore.Qt.NoBrush)
-#        qp.drawRect(0, 0, w-1, h-1)
-
+        self.setStyleSheet("")
+        if self.green:
+            self.setStyleSheet("background-color: rgba(154, 205, 50, %d%s)" % (self.opacity, "%"))
+        if self.red:
+            self.setStyleSheet("background-color: rgba(255, 00, 00, %d%s)" % (self.opacity, "%"))
 
     def getGreen(self):
         return self.green
 
     @QtCore.pyqtSlot(bool)
     def setGreen(self, value):
-        print "setGreen", value
+        #print "setGreen", value
+        self.red = False
+        self.opacity = 100
         if self.green != value:
             self.green = value
             self.emit(QtCore.SIGNAL("greenChanged(bool)"), value)
+            #self.setStyleSheet("QPushButton { background-color: %s }" % "green")
             self.update()
 
     def resetGreen(self):
@@ -66,10 +68,13 @@ class QProgrammButton(QtGui.QPushButton):
 
     @QtCore.pyqtSlot(bool)
     def setRed(self, value):
-        print "setRed", value
+        #print "setRed", value
+        self.green = False
+        self.opacity = 100
         if self.red != value:
             self.red = value
             self.emit(QtCore.SIGNAL("redChanged(bool)"), value)
+            #self.setStyleSheet("QPushButton { background-color: %s }" % "red")
             self.update()
 
     def resetRed(self):
@@ -80,6 +85,11 @@ class QProgrammButton(QtGui.QPushButton):
 
     redLed = QtCore.pyqtProperty (bool, getRed, setRed, resetRed)
     
+    def startFlash(self):
+        self.timer.start()
+
+    def stopFlash(self):
+        self.timer.stop()
 
     # свойство порядковый номер
     def getNumber(self):
